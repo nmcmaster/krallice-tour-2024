44fba78da2a98d1ce6540fd722507e1ff2b1964f
Nicholas McMaster <nmcmaster@gmail.com>
2024-05-08 23:11:50 -0400

    data

--- src/components/Shows.tsx
+++ src/components/Shows.tsx
@@ -1,99 +1,122 @@
 import React from "react";
 
-const activityItems = [
+interface Show {
+	venue: string;
+	link: string;
+	date: string;
+	address: string;
+	cityState: string;
+}
+const shows: Show[] = [
 	{
-		user: {
-			name: "Michael Foster",
-			imageUrl:
-				"https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-		},
-		projectName: "ios-app",
-		commit: "2d89f0c8",
-		branch: "main",
-		date: "1h",
-		dateTime: "2023-01-23T11:00",
+		venue: "Middle East",
+		link: "http://google.com",
+		date: "August 7",
+		address: "Address",
+		cityState: "Cambridge, MA",
 	},
 	{
-		user: {
-			name: "Lindsay Walton",
-			imageUrl:
-				"https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-		},
-		projectName: "mobile-api",
-		commit: "249df660",
-		branch: "main",
-		date: "3h",
-		dateTime: "2023-01-23T09:00",
+		venue: "Space",
+		link: "http://google.com",
+		date: "August 8",
+		address: "Address",
+		cityState: "Portland, ME",
 	},
 	{
-		user: {
-			name: "Courtney Henry",
-			imageUrl:
-				"https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-		},
-		projectName: "ios-app",
-		commit: "11464223",
-		branch: "main",
-		date: "12h",
-		dateTime: "2023-01-23T00:00",
+		venue: "Alchemy",
+		link: "http://google.com",
+		date: "August 9",
+		address: "Address",
+		cityState: "Providence, RI",
 	},
 	{
-		user: {
-			name: "Courtney Henry",
-			imageUrl:
-				"https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-		},
-		projectName: "tailwindui.com",
-		commit: "dad28e95",
-		branch: "main",
-		date: "2d",
-		dateTime: "2023-01-21T13:00",
+		venue: "Space Ballroom",
+		link: "http://google.com",
+		date: "August 10",
+		address: "Address",
+		cityState: "New Haven, CT",
 	},
 	{
-		user: {
-			name: "Michael Foster",
-			imageUrl:
-				"https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-		},
-		projectName: "relay-service",
-		commit: "624bc94c",
-		branch: "main",
-		date: "5d",
-		dateTime: "2023-01-18T12:34",
+		venue: "Trans Pecos",
+		link: "http://google.com",
+		date: "August 11",
+		address: "Address",
+		cityState: "Queens, NY",
+	},
+	{
+		venue: "Underground Arts",
+		link: "http://google.com",
+		date: "August 12",
+		address: "Address",
+		cityState: "Philadelphia, PA",
+	},
+	{
+		venue: "Cobra Cabana",
+		link: "http://google.com",
+		date: "August 13",
+		address: "Address",
+		cityState: "Richmond, VA",
+	},
+	{
+		venue: "Metro",
+		link: "http://google.com",
+		date: "August 14",
+		address: "Address",
+		cityState: "Richmond, VA",
+	},
+	{
+		venue: "Preserving Underground",
+		link: "http://google.com",
+		date: "August 15",
+		address: "Address",
+		cityState: "Richmond, VA",
+	},
+	{
+		venue: "Beachland (tavern)",
+		link: "http://google.com",
+		date: "August 16",
+		address: "Address",
+		cityState: "Richmond, VA",
+	},
+	{
+		venue: "Reggies",
+		link: "http://google.com",
+		date: "August 17",
+		address: "Address",
+		cityState: "Chicago, IL",
 	},
 ];
 
 export default function Shows() {
 	return (
-		<ul role="list" className="divide-y divide-white/5 sm:w-1/2 sm:px-0 px-3 mx-auto">
-			{activityItems.map((item) => (
-				<li key={item.commit} className="py-4">
+		<ul
+			role="list"
+			className="divide-y divide-white/5 sm:w-1/2 lg:w-1/3 sm:px-0 px-4 mx-auto"
+		>
+			{shows.map((show) => (
+				<li key={show.date} className="py-4">
 					<div className="flex items-center gap-x-3">
-						<img
-							src={item.user.imageUrl}
-							alt=""
-							className="h-6 w-6 flex-none rounded-full bg-gray-800"
-						/>
 						<h3 className="flex-auto truncate text-sm font-semibold leading-6 text-white">
-							{item.user.name}
+							{show.venue} (
+							<a href={show.link} className="text-gray-400 underline">
+								TICKETS
+							</a>
+							)
 						</h3>
 						<time
-							dateTime={item.dateTime}
+							dateTime={show.date}
 							className="flex-none text-xs text-gray-500"
 						>
-							{item.date}
+							{show.date}
 						</time>
 					</div>
 					<p className="mt-3 truncate text-sm text-gray-500">
-						Pushed to{" "}
+						<span className="text-gray-400">{show.address}</span>{" "}
+						{/* ( */}
 						<span className="text-gray-400">
-							{item.projectName}
+							{show.cityState}
 						</span>{" "}
-						(
-						<span className="font-mono text-gray-400">
-							{item.commit}
-						</span>{" "}
-						on <span className="text-gray-400">{item.branch}</span>)
+						{/* on <span className="text-gray-400">{show.branch}</span>) */}
 					</p>
 				</li>
 			))}
